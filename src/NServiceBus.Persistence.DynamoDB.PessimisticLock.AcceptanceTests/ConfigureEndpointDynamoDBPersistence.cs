3533ba604216b53eefd0dde9e77d3fea3ff3257e
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-03-09 13:50:45 +0100

    add pessimistic-locking acceptance tests project

---
+++ src/NServiceBus.Persistence.DynamoDB.PessimisticLock.AcceptanceTests/ConfigureEndpointDynamoDBPersistence.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.AcceptanceTests;
+using NServiceBus.Configuration.AdvancedExtensibility;
+
+public class ConfigureEndpointDynamoDBPersistence : IConfigureEndpointTestExecution
+{
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+    {
+        if (configuration.GetSettings().Get<bool>("Endpoint.SendOnly"))
+        {
+            return Task.FromResult(0);
+        }
+
+        // disable installers which are enabled by default in the standard endpoint templates
+        configuration.GetSettings().Set("Installers.Enable", false);
+
+        var persistence = configuration.UsePersistence<DynamoDBPersistence>();
+        persistence.DynamoDBClient(SetupFixture.DynamoDBClient);
+        persistence.TableName(SetupFixture.TableName);
+
+        persistence.Sagas().UsePessimisticLocking = true;
+
+        return Task.FromResult(0);
+    }
+
+    public Task Cleanup() => Task.CompletedTask;
+}
---
+++ src/NServiceBus.Persistence.DynamoDB.PessimisticLock.AcceptanceTests/TestSuiteConstraints.cs
@@ -0,0 +1,18 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using AcceptanceTesting.Support;
+
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc { get; } = false;
+        public bool SupportsCrossQueueTransactions { get; } = true;
+        public bool SupportsNativePubSub { get; } = true;
+        public bool SupportsOutbox { get; } = true;
+        public bool SupportsDelayedDelivery { get; } = true;
+        public bool SupportsPurgeOnStartup { get; } = true;
+
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAcceptanceTestingTransport(true, true);
+
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointDynamoDBPersistence();
+    }
+}
