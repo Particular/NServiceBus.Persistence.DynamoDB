ff7d1f73b4eca9298c727ebac8418875fd6e7afb
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-03-10 10:38:12 +0100

    add timeout verification test

---
+++ src/NServiceBus.Persistence.DynamoDB.PersistenceTests/When_failing_to_acquire_lock.cs
@@ -0,0 +1,61 @@
+﻿namespace NServiceBus.PersistenceTesting
+{
+    using System.Threading.Tasks;
+    using System;
+    using NUnit.Framework;
+    using Sagas;
+
+    public class When_failing_to_acquire_lock : SagaPersisterTests
+    {
+        [Test]
+        public async Task Should_throw_timeout_exception()
+        {
+            configuration.RequiresPessimisticConcurrencySupport();
+
+            var saga = new TestSagaData();
+            await SaveSaga(saga);
+
+            var session1Context = configuration.GetContextBagForSagaStorage();
+            using (var session1 = configuration.CreateStorageSession())
+            {
+                await session1.Open(session1Context);
+
+                // acquire lock
+                var lockedSaga = await configuration.SagaStorage.Get<TestSagaData>(saga.Id, session1, session1Context);
+                Assert.IsNotNull(lockedSaga);
+
+                var session2Context = configuration.GetContextBagForSagaStorage();
+                using (var session2 = configuration.CreateStorageSession())
+                {
+                    await session2.Open(session2Context);
+
+                    // lock is still held by session 1
+                    var exception = Assert.ThrowsAsync<TimeoutException>(() => configuration.SagaStorage.Get<TestSagaData>(saga.Id, session2, session2Context));
+                }
+
+                await session1.CompleteAsync();
+            }
+        }
+
+        public class TestSaga : Saga<TestSagaData>, IAmStartedByMessages<StartTestSagaMessage>
+        {
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<TestSagaData> mapper) => mapper.ConfigureMapping<StartTestSagaMessage>(m => m.SomeId).ToSaga(s => s.SomeId);
+
+            public Task Handle(StartTestSagaMessage message, IMessageHandlerContext context) => throw new NotImplementedException();
+        }
+
+        public class TestSagaData : ContainSagaData
+        {
+            public string SomeId { get; set; } = "Test";
+        }
+
+        public class StartTestSagaMessage : IMessage
+        {
+            public string SomeId { get; set; }
+        }
+
+        public When_failing_to_acquire_lock(TestVariant param) : base(param)
+        {
+        }
+    }
+}
