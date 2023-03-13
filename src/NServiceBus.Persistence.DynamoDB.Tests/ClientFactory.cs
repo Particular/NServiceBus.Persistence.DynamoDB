de8f8f1bf33619ffadf1a6361dd413b5e274e51e
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-03-13 14:57:23 +0100

    support local DynamoDB instance ootb (#212)

--- src/NServiceBus.Persistence.DynamoDB.Tests/ClientFactory.cs
+++ src/NServiceBus.Persistence.DynamoDB.Tests/ClientFactory.cs
@@ -8,8 +8,18 @@ namespace NServiceBus.Persistence.DynamoDB.Tests
     {
         public static IAmazonDynamoDB CreateDynamoDBClient(Action<AmazonDynamoDBConfig> configure = default)
         {
-            var credentials = new EnvironmentVariablesAWSCredentials();
+            AWSCredentials credentials = new EnvironmentVariablesAWSCredentials();
             var config = Create(configure);
+
+            if (string.IsNullOrEmpty(
+                    Environment.GetEnvironmentVariable(
+                        EnvironmentVariablesAWSCredentials.ENVIRONMENT_VARIABLE_ACCESSKEY)))
+            {
+                credentials = new BasicAWSCredentials("localdb", "localdb");
+                config.ServiceURL = Environment.GetEnvironmentVariable("AWS_DYNAMODB_LOCAL_ADDRESS") ??
+                                        "http://localhost:8000";
+            }
+
             return new AmazonDynamoDBClient(credentials, config);
         }
 
