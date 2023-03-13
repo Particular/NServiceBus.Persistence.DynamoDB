5af8a026c591237d7e5a0104802e532cb32826c0
danielmarbach <daniel.marbach@openplace.net>
2023-03-13 16:43:30 +0100

    Write request batcher plus tests

---
+++ src/NServiceBus.Persistence.DynamoDB/Outbox/WriteRequestBatcher.cs
@@ -0,0 +1,36 @@
+#nullable enable
+
+namespace NServiceBus.Persistence.DynamoDB
+{
+    using System;
+    using System.Collections.Generic;
+    using Amazon.DynamoDBv2.Model;
+
+    // Currently placed into outbox folder but could be moved elsewhere once needed in more places.
+    static class WriteRequestBatcher
+    {
+        public static IReadOnlyCollection<List<WriteRequest>> Batch(IReadOnlyCollection<WriteRequest> writeRequests)
+        {
+            var allWriteRequests = new List<List<WriteRequest>>((int)Math.Ceiling(writeRequests.Count / 25d));
+            List<WriteRequest>? currentBatch = null;
+            int index = 0;
+            foreach (var writeRequest in writeRequests)
+            {
+                currentBatch ??= new List<WriteRequest>(25);
+                if (index != 0 && index % 25 == 0)
+                {
+                    allWriteRequests.Add(currentBatch);
+                    currentBatch = new List<WriteRequest>(25);
+                }
+                currentBatch.Add(writeRequest);
+                index++;
+            }
+
+            if (currentBatch?.Count > 0)
+            {
+                allWriteRequests.Add(currentBatch);
+            }
+            return allWriteRequests;
+        }
+    }
+}
