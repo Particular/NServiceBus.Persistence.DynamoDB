161acafc26e054579d048fd5e12a593b200a28e6
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-03-15 18:20:15 +0100

    acceptance test

---
+++ src/NServiceBus.Persistence.DynamoDB.AcceptanceTests/When_configuring_shared_table_without_ttl.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+    using Persistence.DynamoDB;
+
+    public class When_configuring_shared_table_without_ttl : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_fail_startup()
+        {
+            var exception = Assert.ThrowsAsync<InvalidOperationException>(() =>
+                Scenario.Define<ScenarioContext>()
+                    .WithEndpoint<OutboxEndpoint>(e => e
+                        .CustomConfig(c => c
+                            .UsePersistence<DynamoDBPersistence>()
+                            .UseSharedTable(new TableConfiguration
+                            {
+                                TimeToLiveAttributeName = null // using a table without TTL attribute defined
+                            })))
+                    .Done(c => c.EndpointsStarted)
+                    .Run());
+
+            StringAssert.Contains("The outbox persistence table requires a time-to-live attribute to be defined", exception.Message);
+        }
+
+        class OutboxEndpoint : EndpointConfigurationBuilder
+        {
+            public OutboxEndpoint() =>
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
+                    c.EnableOutbox();
+                });
+        }
+    }
+}
