89a014fd4e3c0f33944669cff06013f2173ec4d0
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-03-30 17:06:18 +0200

    add test for TestableDynamoDBSynchronizedStorageSession (#244)

---
+++ src/NServiceBus.Persistence.DynamoDB.Tests/TestableDynamoDBSynchronizedStorageSessionTests.cs
@@ -0,0 +1,43 @@
+﻿namespace NServiceBus.Persistence.DynamoDB.Tests
+{
+    using System.Threading.Tasks;
+    using Amazon.DynamoDBv2.Model;
+    using NServiceBus.Testing;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestableDynamoDBSynchronizedStorageSessionTests
+    {
+        [Test]
+        public async Task Should_be_usable_with_testable_handler_context()
+        {
+            var handler = new Handler();
+            var testableSession = new TestableDynamoDBSynchronizedStorageSession();
+            await handler.Handle(new Handler.MyMessage(), new TestableMessageHandlerContext
+            {
+                SynchronizedStorageSession = testableSession
+            });
+
+            Assert.That(testableSession.TransactWriteItems, Has.Count.EqualTo(3));
+        }
+
+        class Handler : IHandleMessages<Handler.MyMessage>
+        {
+            public Task Handle(MyMessage message, IMessageHandlerContext context)
+            {
+                var session = context.SynchronizedStorageSession.DynamoDBPersistenceSession();
+
+                session.Add(new TransactWriteItem());
+                session.AddRange(new[] { new TransactWriteItem(), new TransactWriteItem() });
+
+                return Task.CompletedTask;
+            }
+
+
+            public class MyMessage
+            {
+            }
+        }
+
+    }
+}
