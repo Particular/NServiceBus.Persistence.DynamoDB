7f31e4048043393d3a6fba98fe69c1aba1220e07
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-04-05 16:17:40 +0200

    throw if session is used after disposal

---
+++ src/NServiceBus.Persistence.DynamoDB.Tests/StorageSessionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.Persistence.DynamoDB.Tests
+{
+    using System;
+    using Amazon.DynamoDBv2.Model;
+    using Extensibility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StorageSessionTests
+    {
+        [Test]
+        public void Should_throw_exception_when_adding_operations_after_dispose()
+        {
+            var session = new StorageSession(new MockDynamoDBClient(), new ContextBag());
+
+            session.Dispose();
+
+            Assert.Throws<ObjectDisposedException>(() => session.Add(new TransactWriteItem()));
+            Assert.Throws<ObjectDisposedException>(() => session.AddRange(new[] { new TransactWriteItem(), new TransactWriteItem() }));
+        }
+    }
+}
