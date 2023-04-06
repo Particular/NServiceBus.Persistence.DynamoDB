6020c6b14725abc821947bd94d0a1d8a064ce810
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-04-06 11:03:05 +0200

    clear actions after first dispose

--- src/NServiceBus.Persistence.DynamoDB/SynchronizedStorage/StorageSession.cs
+++ src/NServiceBus.Persistence.DynamoDB/SynchronizedStorage/StorageSession.cs
@@ -95,6 +95,8 @@
                         Logger.Warn("Failed to cleanup saga locks", e);
                     }
                 }
+
+                CleanupActions.Clear();
             }
         }
 
