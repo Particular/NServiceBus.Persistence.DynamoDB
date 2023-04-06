34d3eb718f00d85d94011554c9f8f15d1eb495f3
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-04-06 10:14:37 +0200

    add additional shared session tests (#257)

---
+++ src/NServiceBus.Persistence.DynamoDB.AcceptanceTests/When_injecting_shared_session.cs
@@ -0,0 +1,87 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using Amazon.DynamoDBv2.Model;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_injecting_shared_session
+    {
+        [Test]
+        public async Task Should_commit_changes_with_session()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointAttachingTransactionOperations>(e => e
+                    .When(c => c.SendLocal(new TriggerMessage())))
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            var items = await SetupFixture.DynamoDBClient.QueryAsync(new QueryRequest
+            {
+                TableName = SetupFixture.TableConfiguration.TableName,
+                ConsistentRead = true,
+                KeyConditionExpression = "#pk = :pk",
+                ExpressionAttributeNames =
+                    new Dictionary<string, string> { { "#pk", SetupFixture.TableConfiguration.PartitionKeyName } },
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue>()
+                {
+                    { ":pk", new AttributeValue(context.ItemPK) }
+                }
+            });
+
+            Assert.AreEqual(1, items.Count);
+        }
+
+        class Context : ScenarioContext
+        {
+            public string ItemPK { get; } = Guid.NewGuid().ToString("N");
+            public bool MessageReceived { get; set; }
+        }
+
+        class EndpointAttachingTransactionOperations : EndpointConfigurationBuilder
+        {
+            public EndpointAttachingTransactionOperations() => EndpointSetup<DefaultServer>();
+
+            class TriggerMessageHandler : IHandleMessages<TriggerMessage>
+            {
+                readonly Context testContext;
+                readonly IDynamoDBStorageSession storageSession;
+
+                public TriggerMessageHandler(Context testContext, IDynamoDBStorageSession storageSession)
+                {
+                    this.testContext = testContext;
+                    this.storageSession = storageSession;
+                }
+
+                public Task Handle(TriggerMessage message, IMessageHandlerContext context)
+                {
+                    storageSession.Add(new TransactWriteItem
+                    {
+                        Put = new Put
+                        {
+                            TableName = SetupFixture.TableConfiguration.TableName,
+                            Item = new Dictionary<string, AttributeValue>
+                            {
+                                {
+                                    SetupFixture.TableConfiguration.PartitionKeyName,
+                                    new AttributeValue(testContext.ItemPK)
+                                },
+                                { SetupFixture.TableConfiguration.SortKeyName, new AttributeValue("Session.Add") },
+                            }
+                        }
+                    });
+
+                    testContext.MessageReceived = true;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
+        class TriggerMessage : IMessage
+        {
+        }
+    }
+}
