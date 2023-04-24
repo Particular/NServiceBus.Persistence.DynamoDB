56199df8db3d75183fa84ada4c1213b0e7151e40
danielmarbach <daniel.marbach@openplace.net>
2023-04-24 14:56:17 +0200

    Remove HttpClientCache hack since the issue has been addressed in Version 3.7.106.15 and higher of Core

--- src/NServiceBus.Persistence.DynamoDB.Tests/ClientFactory.cs
+++ src/NServiceBus.Persistence.DynamoDB.Tests/ClientFactory.cs
@@ -9,7 +9,8 @@ namespace NServiceBus.Persistence.DynamoDB.Tests
         public static IAmazonDynamoDB CreateDynamoDBClient(Action<AmazonDynamoDBConfig> configure = default)
         {
             AWSCredentials credentials = new EnvironmentVariablesAWSCredentials();
-            var config = Create(configure);
+            var config = new AmazonDynamoDBConfig();
+            configure?.Invoke(config);
 
             if (string.IsNullOrEmpty(
                     Environment.GetEnvironmentVariable(
@@ -21,24 +22,6 @@ namespace NServiceBus.Persistence.DynamoDB.Tests
             }
 
             return new AmazonDynamoDBClient(credentials, config);
-        }
-
-        // Can be removed once https://github.com/aws/aws-sdk-net/issues/1929 is addressed by the team
-        // setting the cache size to 1 will significantly improve the throughput on non-windows OSS while
-        // windows had already 1 as the default.
-        // There might be other occurrences of setting this setting explicitly in the code base. Make sure to remove them
-        // consistently once the issue is addressed.
-        // The method is deliberately generic because this config should be set on any SDK client.
-        static TConfig Create<TConfig>(Action<TConfig> configure = default)
-            where TConfig : ClientConfig, new()
-        {
-#if NET
-            var config = new TConfig { HttpClientCacheSize = 1 };
-#else
-            var config = new TConfig();
-#endif
-            configure?.Invoke(config);
-            return config;
         }
     }
 }
